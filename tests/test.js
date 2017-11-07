cd91887417199ca6837ed5ae010f301ba3c492e6
Richard McIntyre <richard.mackstar@gmail.com>
2017-11-07 17:14:08 +0000

    Added rewire.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,9 @@
 import store from '../src/store';
+import AWS from 'aws-sdk';
 
 describe('DynamoDB session store', () => {
   it('should be able to add a session', () => {
+    
 
   });
   it('should be able to get a session', () => {
