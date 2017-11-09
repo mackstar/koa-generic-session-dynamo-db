d61a6754821b0c17d29b563ba3d1e5071e046d9b
Richard McIntyre <richard.mackstar@gmail.com>
2017-11-09 18:16:02 +0000

    Added TTL to shema to make sure sessions are cleared up.

--- src/store.js
+++ src/store.js
@@ -1,31 +1,101 @@
 import AWS from 'aws-sdk';
 import { EventEmitter } from 'events';
 
+const ONE_DAY = 86400 * 1000;
+
 export default class DynamoDBStore extends EventEmitter {
   constructor(options = {}) {
     super();
-    let {
-      client,
+
+    let { connection } = options;
+    const {
       key = 'Id',
       tableName = 'Session',
       credentials,
-      region = 'usa'
+      region = 'usa',
+      ttlKey = 'Ttl',
     } = options;
 
-
-    if (!client) {
-      client = new AWS.DynamoDB({ credentials, region }).DocumentClient();
+    if (!connection) {
+      connection = new AWS.DynamoDB({ credentials, region });
     }
-    this.client = client;
-    this.Key = key;
-    this.TableName = tableName;
+
+    const client = connection.DocumentClient();
+    Object.assign(this, {
+      key,
+      tableName,
+      client,
+      connection,
+      ttlKey,
+    });
+    this.createTable();
+  }
+
+  async doesTableExist() {
+    try {
+      return await this.connection.listTables({ limit: 0 }).promise()
+        .then(result => result.data.TableNames.indexOf(this.TableName) !== -1);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async createTable() {
+    if (await this.doesTableExist()) {
+      return true;
+    }
+
+    const {
+      tableName: TableName,
+      key: AttributeName,
+      readCapacityUnits,
+      writeCapacityUnits,
+    } = this;
+
+    const params = {
+      TableName,
+      KeySchema: [
+        { AttributeName, KeyType: 'HASH' },
+      ],
+      AttributeDefinitions: [
+        { AttributeName, AttributeType: 'S' },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: readCapacityUnits,
+        WriteCapacityUnits: writeCapacityUnits,
+      },
+    };
+
+    try {
+      await this.connection.createTable(params).promise();
+      await this.setTtlField().promise();
+      return true;
+    } catch (err) {
+      throw new Error('Error creating session table');
+    }
+  }
+
+  async setTtlField() {
+    const { tableName: TableName, ttlKey: AttributeName } = this;
+    const params = {
+      TableName,
+      TimeToLiveSpecification: {
+        AttributeName,
+        Enabled: true,
+      },
+    };
+    try {
+      await this.connection.updateTimeToLive(params).promise();
+    } catch (err) {
+      throw new Error('Error setting TTL');
+    }
   }
 
   getParamsForId(id) {
-    const { TableName, Key } = this;
+    const { TableName, key } = this;
     return {
       Key: {
-        [Key]: id,
+        [key]: id,
       },
       TableName,
     };
@@ -36,19 +106,22 @@ export default class DynamoDBStore extends EventEmitter {
     try {
       return await this.client.get(params).promise();
     } catch (err) {
-      return null;
+      throw new Error('Unable to get session.');
     }
   }
 
   async set(id, session, ttl) {
-    const { TableName, Key } = this;
+    const { TableName, key, ttlKey } = this;
+    const maxAge = (session.cookie && session.cookie.maxAge) ? session.cookie.maxAge : null;
+
     const Items = session;
-    Items[Key] = id;
+    Items[key] = id;
+    Items[ttlKey] = new Date((ttl || maxAge || ONE_DAY) + Date.now());
     const params = { TableName, Items };
     try {
       return await this.client.put(params).promise();
     } catch (err) {
-      return null;
+      throw new Error('Unable to set session.');
     }
   }
 
@@ -57,7 +130,7 @@ export default class DynamoDBStore extends EventEmitter {
     try {
       return await this.client.delete(params).promise();
     } catch (err) {
-      return null;
+      throw new Error('Unable to delete session.');
     }
   }
 }
